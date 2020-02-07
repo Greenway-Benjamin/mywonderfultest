147829ef1c77a72a565bd96bdbd151296c0407fb
Greenway-Benjamin <benjamingreenway@gmail.com>
2020-02-07 13:59:03 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ $port = '5432';
 $user = 'lhedqyiaipvdah';
 $dbname = 'd5tf628c1e73sa';
 $password = 'c31b22f594e65a47f7498e694612ea9965ed9c9a8da17464368660de119634fa';
-try{
+
 $pdo = new PDO("pgsql:" . sprintf(
     "host=$host;port=$port;user=$user;password=$password;dbname=$dbname",
     $db["host"],
@@ -15,7 +15,5 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
-} catch {
-echo "ERROR SUCKA";
-}
+
 }
