fc73195446301e61faf0ddf370e4fec8a38168b1
Greenway-Benjamin <benjamingreenway@gmail.com>
2020-02-07 15:22:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,6 @@ $hatsarray = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor();
 
 foreach ($hatsarray as $hat) {
-echo $hatsarray[id] . " and " . $hatsarray[name];
+echo $hatsarray['id'] . " and " . $hatsarray['name'];
 }
 echo 'Hello Sucka';
